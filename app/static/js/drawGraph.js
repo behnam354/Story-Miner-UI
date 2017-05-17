38681e6be0c3b7cf5da96fb10c5bc674042c81b1
Kaicheng <kwang1993@g.ucla.edu>
2017-05-16 19:06:11 -0700

    drawGraph updated
    drawGraph updated

--- app/static/js/drawGraph.js
+++ app/static/js/drawGraph.js
@@ -35,15 +35,18 @@
 
 			// svg
 			var svg = d3.select("div#chartId")
-				.append("div")
-				.classed("svg-container", true) //container class to make it responsive
-				.append("svg")
-				//responsive SVG needs these 2 attributes and no width and height attr
-				.attr("preserveAspectRatio", "xMinYMin meet")
-				.attr("viewBox", "0 0 600 400")
-				//class to make it responsive
-				.classed("svg-content-responsive", true)
-				.call(zoom);
+			   .append("div")
+			   .classed("svg-container", true) //container class to make it responsive
+			   .append("svg")
+			   //responsive SVG needs these 2 attributes and no width and height attr
+			   .attr("preserveAspectRatio", "xMinYMin meet")
+			   .attr("viewBox", "0 0 600 400")
+			   //class to make it responsive
+			   .classed("svg-content-responsive", true)
+				  .attr("width", "100%")
+				  .attr("height", "100%")
+				  .call(zoom)
+				  .append("g");
 
 
 			// force layout
