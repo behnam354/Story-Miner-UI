8673657e51c46e2dfb8da7db2b3ae35105aeac49
Kaicheng <kwang1993@g.ucla.edu>
2017-05-24 15:05:14 -0700

    daemon added
    daemon added

--- serve.py
+++ serve.py
@@ -1,2 +1,26 @@
 from app import app
-app.run(host = '0.0.0.0', port='5000')
+from daemon import Daemon
+
+
+
+class MyDaemon(Daemon):
+    def run(self):
+        while True:
+            app.run(host = '0.0.0.0', port='5000')
+            
+if __name__ == "__main__":
+    daemon = MyDaemon('/tmp/serve.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
