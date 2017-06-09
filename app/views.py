2181272514c5ea07926de1e5e214da35ce81c40d
Kaicheng <kwang1993@g.ucla.edu>
2017-06-09 00:40:46 -0700

    No duplicates

--- app/views.py
+++ app/views.py
@@ -20,20 +20,20 @@ if not app.config['USE_NEW_PATH'] \
    or not app.config['MAIN_FUNCTIONS_DIR_NEW'] \
    or not app.config['DATA_DIR_NEW']:
 
-    print "main_function_dir_default"
-    main_functions_dir = os.path.join(curdir, app.config['MAIN_FUNCTIONS_DIR_DEFAULT'])
-    print "data_dir_default"
-    data_dir = os.path.join(curdir, app.config['DATA_DIR_DEFAULT'])
+	print "main_function_dir_default"
+	main_functions_dir = os.path.join(curdir, app.config['MAIN_FUNCTIONS_DIR_DEFAULT'])
+	print "data_dir_default"
+	data_dir = os.path.join(curdir, app.config['DATA_DIR_DEFAULT'])
 
 
 else:
-    print "main_function_dir_new"
-    main_functions_dir = app.config['MAIN_FUNCTIONS_DIR_NEW']
-    print "main_function_dir_new"
-    data_dir = app.config['DATA_DIR_NEW']
+	print "main_function_dir_new"
+	main_functions_dir = app.config['MAIN_FUNCTIONS_DIR_NEW']
+	print "main_function_dir_new"
+	data_dir = app.config['DATA_DIR_NEW']
 
 
-    
+	
 sys.path.insert(0, main_functions_dir)
 from main_functions import *
 
@@ -62,154 +62,177 @@ LOAD_ANNOTATIONS = app.config['LOAD_ANNOTATIONS']
 @app.route('/' , methods=['GET', 'POST'])
 
 def index():
-    form = InputForm()
-    if form.validate_on_submit():
-        #try:
-            tables = None
-            titles = None
-            graph = None
-            graphTitle = None
-            state = None
-            checkTable = [form.showRels.data,
-                        form.rankRels.data,
-                        form.rankEntities.data]
-            checkGraph = form.showGraph.data
-            
-            output = getOutput(form.text.data)
-            if output:
-                tables, titles, graph, graphTitle = output
-                tables = [tables[i] for i, v in enumerate(checkTable) if v == True]
-                titles = [titles[i] for i, v in enumerate(checkTable) if v == True]
-                if not checkGraph:
-                    graph = None
-                    graphTitle = None
-                state = "Succeeded!"
-            else:
-                state = "No relationships extracted." 
-            return render_template('index.html',
-                                        form = form,
-                                        tables = tables,
-                                        titles = titles,
-                                        graph = graph,
-                                        graphTitle = graphTitle,
-                                        state = state
-                                        )    
-        #except:
-        #   return abort(500)
+	form = InputForm()
+	if form.validate_on_submit():
+		#try:
+			tables = None
+			titles = None
+			graph = None
+			graphTitle = None
+			state = None
+			checkTable = [form.showRels.data,
+						form.rankRels.data,
+						form.rankEntities.data]
+			checkGraph = form.showGraph.data
+			
+			output = getOutput(form.text.data)
+			if output:
+				tables, titles, graph, graphTitle = output
+				tables = [tables[i] for i, v in enumerate(checkTable) if v == True]
+				titles = [titles[i] for i, v in enumerate(checkTable) if v == True]
+				if not checkGraph:
+					graph = None
+					graphTitle = None
+				state = "Succeeded!"
+			else:
+				state = "No relationships extracted." 
+			return render_template('index.html',
+                                                form = form,
+                                                tables = tables,
+                                                titles = titles,
+                                                graph = graph,
+                                                graphTitle = graphTitle,
+                                                state = state
+                                                )	
+		#except:
+		#   return abort(500)
 
-    return render_template('index.html', form = form)
+	return render_template('index.html', form = form)
 
 
 def get_top_rels(all_rels,top_num=-1):
-    df = pd.DataFrame(columns = ['rel', 'count'])
-    cnt = Counter()
-    for r in all_rels:
-        cnt[r] += 1
-    if top_num == -1: # means print all
-        print "Frequent relations:"
-        for letter,count in cnt.most_common():
-            print letter, ": ", count
-            df.loc[len(df)] = [letter, count]
-    else:
-        print "top ", top_num, " frequent relations:"
-        for letter,count in cnt.most_common(top_num):
-            print letter, ": ", count
-            df.loc[len(df)] = [letter, count]
+	df = pd.DataFrame(columns = ['rel', 'count'])
+	cnt = Counter()
+	for r in all_rels:
+		cnt[r] += 1
+	if top_num == -1: # means print all
+		print "Frequent relations:"
+		for letter,count in cnt.most_common():
+			print letter, ": ", count
+			df.loc[len(df)] = [letter, count]
+	else:
+		print "top ", top_num, " frequent relations:"
+		for letter,count in cnt.most_common(top_num):
+			print letter, ": ", count
+			df.loc[len(df)] = [letter, count]
 
-    df["count"] = df["count"].astype(np.int64)
-    return df
+	df["count"] = df["count"].astype(np.int64)
+	return df
 
 def count_entities(entities,top_num=-1):
-    df = pd.DataFrame(columns = ['entity', 'count'])
-    cnt = Counter()
-    for r in entities:
-        cnt[r] += 1
-    if top_num == -1: # means print all   
-        print "Frequent entities:"
-        for letter,count in cnt.most_common():
-            print letter, ": ", count
-            df.loc[len(df)] = [letter, count]
-    else:
-        print "top ", top_num, " frequent entities:"
-        for letter,count in cnt.most_common(top_num):
-            print letter, ": ", count
-            df.loc[len(df)] = [letter, count]
+	df = pd.DataFrame(columns = ['entity', 'count'])
+	cnt = Counter()
+	for r in entities:
+		cnt[r] += 1
+	if top_num == -1: # means print all   
+		print "Frequent entities:"
+		for letter,count in cnt.most_common():
+			print letter, ": ", count
+			df.loc[len(df)] = [letter, count]
+	else:
+		print "top ", top_num, " frequent entities:"
+		for letter,count in cnt.most_common(top_num):
+			print letter, ": ", count
+			df.loc[len(df)] = [letter, count]
 
-    df["count"] = df["count"].astype(np.int64)
-    return df
+	df["count"] = df["count"].astype(np.int64)
+	return df
+
+def getDistinctRels(df_rels):
+        if len(df_rels) <= 0:
+            return df_rels
+
+        df_keep = df_rels[['arg1', 'arg2','rel']].groupby(['arg1', 'arg2']).agg(lambda x: x.value_counts().index[0])
+        df_keep.reset_index(inplace = 1)
+        df_keep = df_keep.reindex(columns = ['arg1', 'arg2','rel'])
+        keep = df_keep.values.tolist()
+
+        for index, row in df_rels.iterrows():
+            if [row['arg1'], row['arg2'], row['rel']] not in keep:
+                df_rels.drop(index, inplace = 1)
+        return df_rels
+
 
 def getOutput(text):
-    text = text.encode('utf8')
-    print text
-    if '\r\n' in text:
-        texts = text.split('\r\n')
-    elif '\n' in text:
-        texts = text.split('\n')
-    else:
-        texts = [text]
-    print texts
+	text = text.encode('utf8')
+	print text
+	if '\r\n' in text:
+		texts = text.split('\r\n')
+	elif '\n' in text:
+		texts = text.split('\n')
+	else:
+		texts = [text]
+	print texts
 
-    input_fname = 'tweets_textOnly_sample.txt'
-    file_input_arg = data_dir + input_fname
-    output_dir_arg = data_dir
-    start_time = time.time()
+	input_fname = 'tweets_textOnly_sample.txt'
+	file_input_arg = data_dir + input_fname
+	output_dir_arg = data_dir
+	start_time = time.time()
 
-    all_rels_str, all_rels, output = text_corpus_to_rels(file_input_arg,
-                                                         DATA_SET,
-                                                         INPUT_DELIMITER,
-                                                         input_fname,
-                                                         output_dir_arg,
-                                                         MAX_ITERATION,
-                                                         CLEAN_SENTENCES,
-                                                         SEPARATE_SENT,
-                                                         SHOW_DP_PLOTS,
-                                                         SHOW_REL_EXTRACTIONS,
-                                                         SAVE_ALL_RELS,
-                                                         EXTRACT_NESTED_PREPOSITIONS_RELS,
-                                                         SAVE_ANNOTATIONS_TO_FILE,
-                                                         LOAD_ANNOTATIONS,
-                                                         texts = texts
-                                                        )   
-                                                                                                            
-    end_time = time.time()
-    print "Relation Extraction Time: ", end_time-start_time , "(seconds) - ", (end_time-start_time)/60, "(min)"
-    print "Total number of extracted relations: ", len(all_rels_str)
-    df_top = get_top_rels(all_rels_str,top_num=-1) 
-    df_rels = pd.DataFrame(all_rels)
-    df_output = pd.DataFrame(output)
-    print df_rels
+	all_rels_str, all_rels, output = text_corpus_to_rels(file_input_arg,
+                                                             DATA_SET,
+                                                             INPUT_DELIMITER,
+                                                             input_fname,
+                                                             output_dir_arg,
+                                                             MAX_ITERATION,
+                                                             CLEAN_SENTENCES,
+                                                             SEPARATE_SENT,
+                                                             SHOW_DP_PLOTS,
+                                                             SHOW_REL_EXTRACTIONS,
+                                                             SAVE_ALL_RELS,
+                                                             EXTRACT_NESTED_PREPOSITIONS_RELS,
+                                                             SAVE_ANNOTATIONS_TO_FILE,
+                                                             LOAD_ANNOTATIONS,
+                                                             texts = texts
+                                                            )   
+																											
+	end_time = time.time()
+	print "Relation Extraction Time: ", end_time-start_time , "(seconds) - ", (end_time-start_time)/60, "(min)"
+	print "Total number of extracted relations: ", len(all_rels_str)
+	df_top = get_top_rels(all_rels_str,top_num=-1) 
+	df_rels = pd.DataFrame(all_rels)
+	#df_output = pd.DataFrame(output)
+	
+        print "**************** df_rels ****************"
+	print df_rels
 
-    if len(df_rels):
-        print len(df_rels)
-        g_arg = rels_to_network(df_rels,
-                            input_fname,
-                            output_dir_arg,
-                            MAX_ITERATION,
-                            NODE_SELECTION,
-                            DATA_SET,
-                            SAVE_GEFX,
-                            SAVE_PAIRWISE_RELS,
-                            SHOW_ARGUMENT_GRAPH
-                           )
+	if len(df_rels):
+		print len(df_rels)
+		df_rels_distinct = getDistinctRels(df_rels)
+		print "**************** df_rels_distinct ****************"
+		print df_rels_distinct
+		g_arg = rels_to_network(df_rels_distinct,
+                                        input_fname,
+                                        output_dir_arg,
+                                        MAX_ITERATION,
+                                        NODE_SELECTION,
+                                        DATA_SET,
+                                        SAVE_GEFX,
+                                        SAVE_PAIRWISE_RELS,
+                                        SHOW_ARGUMENT_GRAPH   
+                                   )
 
-        entities = list(df_rels['arg1']) + list(df_rels['arg2'])
-        df_entities = count_entities(entities,top_num=-1) 
-        classes = 'table table-bordered table-hover table-striped '
-        return [df_rels[['arg1', 'rel', 'arg2']].to_html(classes=classes),
-            df_top.to_html(classes=classes),
-            df_entities.to_html(classes=classes)
-            ], ["Extracted Relationships",
-                "Ranking of the Extractions",
-                "Ranking of the Entities"], json.dumps(node_link_data(g_arg)), 'Graph'
-    else:
-        return None
-        
+		entities = list(df_rels['arg1']) + list(df_rels['arg2'])
+		df_entities = count_entities(entities,top_num=-1) 
+		classes = 'table table-bordered table-hover table-striped '
+		return [df_rels[['arg1', 'rel', 'arg2']].to_html(classes=classes),
+			df_top.to_html(classes=classes),
+			df_entities.to_html(classes=classes)
+			], ["Extracted Relationships",
+				"Ranking of the Extractions",
+				"Ranking of the Entities"], json.dumps(node_link_data(g_arg)), 'Graph'
+	else:
+		return None
+
+
+
+
 @app.errorhandler(500)
 
 def page_not_found(e):
-    return render_template('500.html'), 500   
+	return render_template('500.html'), 500   
 
 @app.errorhandler(404)
 
 def page_not_found(e):
-    return render_template('404.html'), 404   
+	return render_template('404.html'), 404   
